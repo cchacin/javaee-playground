0da928d2af355f74ca97402a2803ea7bf8068b91
Carlos Chacin <cchacin@gmail.com>
2017-01-10 15:15:19 -0800

    Refactor

---
+++ src/test/java/com.airhacks/MyResourceTest.java
@@ -0,0 +1,16 @@
+package com.airhacks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class MyResourceTest extends Assertions {
+
+    @Test
+    public void testHappyPath() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
